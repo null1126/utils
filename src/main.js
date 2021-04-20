2c3f472218bdd7c0459e3ff0e4344694070ccea9
tianjie <384434682@qq.com>
2021-04-20 16:06:01 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -1,11 +1,7 @@
-// import verify from './verify/index.js'
-// import tools from './tools/index.js'
+import verify from './verify/index.js'
+import tools from './tools/index.js'
 
-// export default {
-//     verify,
-//     tools
-// }
-
-// exports.verify = verify
-
-module.exports = require('./verify/index.js')
+export default {
+    verify,
+    tools
+}
