eb69b18bf80178223054b28651d247aa3dddf9e9
tianjie <384434682@qq.com>
2021-04-20 15:49:04 +0800

    测试

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
-import verify from './verify/index.js'
-import tools from './tools/index.js'
+// import verify from './verify/index.js'
+// import tools from './tools/index.js'
 
 // export default {
 //     verify,
 //     tools
 // }
 
-exports.verify = verify
+// exports.verify = verify
+
+module.exports = require('./verify/index.js')
